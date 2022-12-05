f2ad001c5805acf8be868be02a2f889704ffe63c
aiglesiasp <iglesiaspubillaitor@gmail.com>
2022-12-05 20:27:18 +0100

    Working project

---
+++ kcHeroesMulti/kcHeroesMulti/Views/Login/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  kcHeroesMulti
+//
+//  Created by Aitor Iglesias Pubill on 5/12/22.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
