5017e33ebde09410e7a8dc80a26165d0b3ddab92
aiglesiasp <iglesiaspubillaitor@gmail.com>
2022-12-06 13:54:58 +0100

    KeyChain creado

---
+++ kcHeroesMulti/kcHeroesMulti/Tools/KeyChain.swift
@@ -0,0 +1,31 @@
+//
+//  KeyChain.swift
+//  kcHeroesMulti
+//
+//  Created by Aitor Iglesias Pubill on 6/12/22.
+//
+
+import KeychainSwift
+//MARK: FUNCION SAVE
+func saveKC(key: String, value: String) -> Bool {
+    if let data = value.data(using: .utf8) {
+        let keychain = KeychainSwift()
+        keychain.set(data, forKey: key)
+        return true
+    } else  {
+        return false
+    }
+}
+//MARK: FUNCION READ
+func loadKC(key: String) -> String? {
+    let keychain = KeychainSwift()
+    if let data = keychain.get(key) {
+        return data
+    }
+    return ""
+}
+//MARK: FUNCION ELIMINAR
+func deleteKC(key: String){
+    let keychain = KeychainSwift()
+    keychain.delete(key)
+}
--- kcHeroesMulti/kcHeroesMulti/Views/Login/LoginView.swift
+++ kcHeroesMulti/kcHeroesMulti/Views/Login/LoginView.swift
@@ -9,12 +9,31 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            Text("ENTRAR")
+                .foregroundColor(.yellow)
+                .bold()
+                .frame(width: 200, height: 200)
+                .font(.title)
+            Spacer()
+            Text("Hola me llamo")
+                .background(.red)
+            
+            Spacer()
+        }
+        
     }
 }
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
         LoginView()
+            .environment(\.locale, .init(identifier: "es"))
+        
+        LoginView()
+            .environment(\.locale, .init(identifier: "en"))
     }
+    
+    
 }
