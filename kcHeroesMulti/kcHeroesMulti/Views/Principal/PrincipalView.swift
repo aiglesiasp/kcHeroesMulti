5a2016f82de9ca63f053e59ee77b315cfd20fbca
aiglesiasp <iglesiaspubillaitor@gmail.com>
2022-12-07 18:57:19 +0100

    PrincipalView initial

---
+++ kcHeroesMulti/kcHeroesMulti/Views/Principal/PrincipalView.swift
@@ -0,0 +1,20 @@
+//
+//  PrincipalView.swift
+//  kcHeroesMulti
+//
+//  Created by Aitor Iglesias Pubill on 7/12/22.
+//
+
+import SwiftUI
+
+struct PrincipalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PrincipalView_Previews: PreviewProvider {
+    static var previews: some View {
+        PrincipalView()
+    }
+}
